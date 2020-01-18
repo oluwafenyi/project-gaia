177dd7ae65def2e05eeb72297d8ad9954f085e47
Enyioma Osondu <o.enyioma@gmail.com>
2020-01-18 17:50:45 +0100

    modified course model

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,6 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'courses.apps.CoursesConfig',
+    'people.apps.PeopleConfig',
 ]
 
 MIDDLEWARE = [
--- courses/models.py
+++ courses/models.py
@@ -3,8 +3,19 @@ from django.db import models
 
 
 class Course(models.Model):
+    GEOLOGY = 1
+    GEOPHYSICS = 2
+    OTHERS = 3
+
+    CATEGORIES = (
+        (GEOLOGY, 'Geology'),
+        (GEOPHYSICS, 'Geophysics'),
+        (OTHERS, 'Others'),
+    )
+
     title = models.CharField(max_length=400)
     code = models.CharField(max_length=5, unique=True)
+    category = models.CharField(choices=CATEGORIES, max_length=2)
     units = models.PositiveIntegerField()
     description = models.TextField()
     drive_link = models.URLField()
