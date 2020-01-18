04099be376e359f19a1bf6828773e2522613cda0
Enyioma Osondu <o.enyioma@gmail.com>
2020-01-18 03:21:06 +0100

    serving up the contact page

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from gaia.views import HomePageView
+from gaia.views import HomePageView, ContactPageView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- gaia/views.py
+++ gaia/views.py
@@ -7,3 +7,9 @@ class HomePageView(View):
 
     def get(self, request):
         return render(request, 'gaia/index.html')
+
+
+class ContactPageView(View):
+
+    def get(self, request):
+        return render(request, 'gaia/contact.html')
