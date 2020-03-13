cc2349531c65ffae269d701d1f8b3519a1244b66
Enyioma Osondu <o.enyioma@gmail.com>
2020-03-13 18:36:56 +0100

    wrote data migration for course links

--- courses/models.py
+++ courses/models.py
@@ -19,8 +19,8 @@ class Course(models.Model):
     title = models.CharField(max_length=400)
     code = models.CharField(max_length=6, unique=True)
     category = models.CharField(choices=CATEGORIES, max_length=2)
-    units = models.PositiveIntegerField()
-    description = models.TextField()
+    units = models.PositiveIntegerField(null=True)
+    description = models.TextField(blank=True)
     drive_link = models.URLField(blank=True)
     lecturers = models.ManyToManyField(Lecturer, related_name='courses')
 
